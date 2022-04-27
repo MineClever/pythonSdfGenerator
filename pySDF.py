135868981951c4937b3dd65bbe41fc9135e29f45
MineClever <36216195+MineClever@users.noreply.github.com>
2022-04-28 01:13:13 +0800

    # add codes

--- pySDF.py
+++ pySDF.py
@@ -1,81 +1,195 @@
 # _*_ coding=utf-8 _*_
 
-import os
+
+import os, sys
 import cv2 as cv2
 import numpy as np
 
+# def type
+class Vector2 ():
+    def __init__(self, x=0,y=0):
+        self.data = np.zeros(2, dtype=float)
+        self.x = x
+        self.y = y
 
-# Distance around a pixel
-# ##################### #
-# sqrt(2)   1   sqrt(2) #
-#       1   0   1       #
-# sqrt(2)   1   sqrt(2) #
-# ##################### #
+    @property
+    def x (self):
+        return self.data[0]
 
-# now.sdf = lambed now : [(near.sdf + distance(now,near)) for near in aroundPx(now)].min
+    @x.setter
+    def x (self, value):
+        self.data[0] = float(value)
+        
+    @property
+    def y (self):
+        return self.data[1]
 
-#ref to https://github.com/Lisapple/8SSEDT
+    @x.setter
+    def y (self, value):
+        self.data[1] = float(value)
 
+    def __add__ (self, value):
+        return Vector2((self.x+value.x),(self.y+value.y))
 
-# ref to https://blog.csdn.net/tianwaifeimao/article/details/45078661
-# 欧几里得距离转换 (Euclidean Distance Transform, EDT) 
+    def length_squared (self) -> float:
+        return (self.x*self.x + self.y*self.y)
 
-# ref to https://zhuanlan.zhihu.com/p/337944099
-# How to generate a SDF
-
-# now.sdf = 999999;
-# if(now in object){
-#     now.sdf = 0;
-# }else{
-#     foreach(near in nearPixel(now)){
-#         now.sdf = min(now.sdf,near.sdf + distance(now,near));
-#     }
-# }
-
-
-
-
-class GlobalVar ():
-    mWidth = 256
-    mHeight = 256
-
-class Point () :
+    def length (self) -> float:
+        return np.sqrt(self.length_squared())
     
-    def __init__ (self , pX ,pY):
-        self.mDx = pX
-        self.mDy = pY
+# For a bitmap with representation:
+# [0][0][0]
+# [0][1][1]
+# [1][1][1]
 
-    def DistSqrt (self):
-        return self.__mDx**2 + self.__mDy**2
+# using relative offset [offset x, offset y] :
+# [-1,-1][0,-1][1,-1]
+# [-1, 0][0, 0][1, 0]
+# [-1, 1][0, 1][1, 1]
 
-class Grid ():
-    # set a grid which include a Point matrix
-    def __init__ (self, pWidth:int , pHeight:int):
-        self.mGrid = [[Point(0,0)]*pWidth]*pHeight
-        for x in range(pWidth):
-            for y in range(pHeight):
-                self.mGrid[pWidth][pHeight] = Point(pWidth, pHeight)
+class SSEDT8 (object):
+
+    class Grid ():
+        
+        def __init__(self, width:int , height:int):  
+            self.width = width
+            self.height = height
+            self.size = Vector2(width,height)
+            self.distances = [Vector2()]* (width*height)
+        
+        def has (self, x:int, y:int) -> bool:
+            return (0 <= x and x < self.size.x and 0 <= self.size.y and y < self.size.y)
+
+        def _index (self, x:int, y:int) -> int:
+            return (y * self.size.x + x)
 
 
-lInside = Point(0,0)
-lEmpty = Point(9999,9999)
-lGridA = Grid (GlobalVar.mWidth,GlobalVar.mHeight)
-lGridB = Grid (GlobalVar.mWidth,GlobalVar.mHeight)
+        def get_size(self) -> Vector2:
+            return self.size
+
+        def get_dist(self, x:int, y:int) -> Vector2:
+            return self.distances[self._index(x,y)]
+
+        def set_dist(self, x:int, y:int, p_dinstance:Vector2) :
+            self.distances[self._index(x,y)] = p_dinstance
+
+        def update (self, x:int, y:int, offset:Vector2):
+            pos = Vector2(x,y)
+            offset_pos = pos + offset
+            distance = self.get_dist(x, y)
+            dist_sq = distance.length_squared()
+            
+            if (self.has(offset_pos.x, offset_pos.y)):
+                offset_dist = self.get_dist(offset_pos.x, offset_pos.y) + offset
+                offset_sq = offset_dist.length_squared()
+                if (offset_sq < dist_sq):
+                    self.set_dist(x, y, offset_dist)
 
 
-def getPoint (pGrid : Grid , pX: int, pY: int):
-    if (pX >=0 and pY >=0 and pX < GlobalVar.mWidth and pY < GlobalVar.mHeight):
-        return pGrid.mGrid[pX][pY]
-    else :
-        return lEmpty
+    @classmethod
+    def apply_offsets (cls, p_grid : Grid,x :int ,y :int, p_offsets:list):
+        size = p_offsets.size
+        for i in range(size):
+            p_grid.update(x,y,p_offsets[i])
     
-def putPoint (pGrid : Grid , pX: int, pY: int , pPoint :Point):
-    pGrid.mGrid[pX][pY] = pPoint
+    @classmethod
+    def apply_pass (cls, p_grid : Grid, p_offsets1 : list, p_offsets2 : list, inverted=False):
+        grid_size = p_grid.get_size()
+        width = grid_size.x
+        height = grid_size.y
+        if (inverted):
+            y = height - 1
+            x = width - 1
+            while (y > 0):
+                while (x >= 0):
+                    cls.apply_offsets(p_grid, x, y, p_offsets1)
+                    x -= 1
+                else:
+                    x = 0
+                while (x < width):
+                    cls.apply_offsets(p_grid, x, y, p_offsets2)
+                    x += 1
+                y -= 1
+                    
+        
 
-def comparePoint (pGrid : Grid , pPoint:list[Point], pX: int, pY: int , pOffsetX:int , pOffsetY :int):
-    lAroundPoint = getPoint(pGrid, pX+pOffsetX , pY+pOffsetY)
-    lAroundPoint.mDx += pOffsetX
-    lAroundPoint.mDy += pOffsetY
+    @staticmethod
+    def _bind_methods():
+        pass
 
-    if (lAroundPoint.DistSqrt() < pPoint.DistSqrt()):
-        pPoint[0] = lAroundPoint
+    @classmethod
+    def do_sdf (cls, p_input_image_path='',p_output_image_path='', scale = 0.005):
+        # read img by openCV
+        img = cv2.imread(p_input_image_path)
+        width = img.shape[0]
+        height = img.shape[1]
+
+        # Initialise grids
+        grid1 = cls.Grid(width,height)
+        grid2 = cls.Grid(width,height)
+        DISTANT = sys.maxsize
+
+        for y in range(height):
+            for x in range(width):
+                distance = 0 if img[x][y][2] > 0 else DISTANT
+                grid1.set_dist(x, y, Vector2(distance, distance))
+                grid2.set_dist(x, y, Vector2(DISTANT - distance, DISTANT - distance))
+
+
+        # using relative offset [offset x, offset y] :
+        # [-1,-1][0,-1][1,-1]
+        # [-1, 0][0, 0][1, 0]
+        # [-1, 1][0, 1][1, 1]
+        
+        # Pass 1
+
+        # [2] [1] [ ]
+        # [0] [ ] [ ]
+        # [3] [ ] [ ]
+        offsets1 = list()
+        offsets1.append(Vector2(-1, 0))     # 0
+        offsets1.append(Vector2(0, -1))     # 1
+        offsets1.append(Vector2(-1, -1))    # 2 
+        offsets1.append(Vector2(1, -1))     # 3
+        
+        # [ ] [ ] [ ]
+        # [ ] [ ] [0]
+        # [ ] [ ] [ ]
+        offsets2 = list()
+        offsets1.append(Vector2(1, 0))      # 0
+        
+        cls.apply_pass(grid1 , offsets1 ,offsets2 ,False)
+        cls.apply_pass(grid2 , offsets1 ,offsets2 ,False)
+
+        # Pass 2
+
+        # [ ] [ ] [ ]
+        # [ ] [ ] [0]
+        # [2] [1] [3]
+        offsets1.clear()
+        offsets1.append(Vector2(1, 0))  # 0
+        offsets1.append(Vector2(0, 1))  # 1
+        offsets1.append(Vector2(-1, 1)) # 2
+        offsets1.append(Vector2(1, 1))  # 3
+
+        # [ ] [ ] [ ]
+        # [0] [ ] [ ]
+        # [ ] [ ] [ ]
+        offsets2.clear()
+        offsets2.append(Vector2(-1, 0)) # 0
+
+
+        # make Img data
+        out_img = np.zeros((width,height),dtype=np.float32)
+        for y in range(height):
+            for x in range(width):
+                distance1 = grid1.get_dist(x, y)
+                distance2 = grid2.get_dist(x, y)
+                distance = distance1.length() - distance2.length()
+                distance = (1 + max(-1, min(distance * scale, 1))) / 2.0
+                out_img[x][y] = distance
+
+        cv2.imshow("SDF Image", out_img.astype(np.uint8))
+        
+        #cv2.imwrite(p_output_image_path,out_img.astype(np.uint8))
+        
